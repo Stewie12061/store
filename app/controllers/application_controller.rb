cd2a81e533d7611c96398ddf7100153473e9d874
nhut170501 <nhut170501@gmail.com>
2021-04-26 20:14:44 +0700

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  
   private
   def set_locale
-    I18n.locale = params[:locale].to_sym || :en
+    #params[:locale] ||= :en
+    #I18n.locale = params[:locale].to_sym || :en
+
+    #params[:locale] ||= cookies[:locale]
+    #cookies[:locale] ||= :en
+    #I18n.locale = params[:locale].to_sym || :en
+
+    
+    cookies[:locale] ||= :en
+    I18n.locale = cookies[:locale].to_sym || :en
   end
-end
+ end
+ 
