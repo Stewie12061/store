ce1e10f8ad69dfc0cf28abdf53952933a583557d
nhut170501 <nhut170501@gmail.com>
2021-05-03 21:22:53 +0700

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :accessories
+  resources :about
+  resources :news
+
+  
   # devise_for :users
   devise_for :users, controllers: {
         sessions: 'users/sessions',
@@ -6,5 +12,6 @@ Rails.application.routes.draw do
         registrations: 'users/registrations'
       }
   root 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
